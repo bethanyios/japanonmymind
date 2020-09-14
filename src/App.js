75bec98553fc37ce491cb7f1c6394d3dfbbc4be0
Bethany Scott <bethanyioscott@hotmail.co.uk>
2020-09-14 16:10:35 +0100

    Set up placeholder page

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './placeholder.svg';
+import './App.css';
+
+const App {
+  return (
+    <div className="home-container">
+      <header className="home-body">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Lucy is building her website and will have something nice for you soon
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
